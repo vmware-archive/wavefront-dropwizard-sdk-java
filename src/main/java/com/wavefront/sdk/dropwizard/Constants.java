daf7f4a145baec917f6a7afdf5cf06fa92a9a9eb
sushantdewan123 <sushant@wavefront.com>
2018-12-27 13:32:21 -0800

    Created a first class SDK for out of the box metrics about your dropwizard app

---
+++ src/main/java/com/wavefront/sdk/dropwizard/Constants.java
@@ -0,0 +1,14 @@
+package com.wavefront.sdk.dropwizard;
+
+/**
+ * Dropwizard SDK constants.
+ *
+ * @author Sushant Dewan (sushant@wavefront.com).
+ */
+public class Constants {
+
+  /**
+   * Name of the Dropwizard SDK component.
+   */
+  public final static String DROPWIZARD_COMPONENT = "dropwizard";
+}
