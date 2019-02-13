52f29a9bd0e49d7cf382627145eb16dc4db30ec2
sushantdewan123 <sushant@wavefront.com>
2019-02-13 12:33:51 -0800

    Optimize unused imports

--- src/main/java/com/wavefront/sdk/dropwizard/reporter/WavefrontDropwizardReporter.java
+++ src/main/java/com/wavefront/sdk/dropwizard/reporter/WavefrontDropwizardReporter.java
@@ -9,7 +9,6 @@ import com.wavefront.sdk.entities.metrics.WavefrontMetricSender;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
